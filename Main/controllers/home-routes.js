0e7d3cf95cbae3b2212716e683e009ddf18413f9
Nate Worley <immythz@gmail.com>
2021-07-07 01:22:37 -0500

    added login and signUp handlebars, added routes for the main pags, as well as edit main and homepage handlebars

--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -16,4 +16,50 @@ router.get('/', async (req, res) => {
     }
 })
 
+// Render signUp page if not loggedIn
+router.get('/signUp', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/')
+        return;
+
+    }   else {
+        res.render('signUp')
+    }
+})
+
+// Renders login screen if user is not logged in
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/')
+        return;
+
+    }   else {
+        res.render('login')
+    }
+})
+
+// Renders a single blog post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment, 
+                    include: [User],
+                },
+            ],
+        });
+
+        if (postData) {
+            const post = postData.get({ plain: true })
+            res.render('post', { post })
+        } else {
+            res.status(404).end()
+        }
+    }   catch (err){
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router;
