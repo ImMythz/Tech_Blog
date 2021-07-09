f1ea59cdc33b0f66c24fb65fa568b8366905909a
Nate Worley <immythz@gmail.com>
2021-07-09 13:45:30 -0500

    modified dbConfig.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "us-cdbr-east-04.cleardb.com",
     USER: "b4af61eef454f4",
-    PASSWORD: "725b810",
+    PASSWORD: "a725b810",
     DB: "heroku_75ca7dc5733abe3"
 };  
 
