2aa2281dba0bf1ca7b7d9e249b82093f91f90979
Nate Worley <immythz@gmail.com>
2024-04-10 11:49:50 -0500

    test chnage

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,5 @@
 // Handles whether the user is logged in and if they user is not, redirects them to the login page
+// This is a comment
 const withAuth = (req, res, next) => {
   if (!req.session.loggedIn) {
     res.redirect('/login')
